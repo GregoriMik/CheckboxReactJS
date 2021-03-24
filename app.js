e188480d272a7046f98f902a58c7cd3f16503731
Gregori Mikina <gregorimikina@gmail.com>
2021-03-24 23:24:23 +0100

    Verify and display in the console that the check box is selected

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const PositiveMessage = () => <p>Możesz obejrzeć film</p>;
 
-const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu</p>;
+const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat</p>;
 
 
 class CheckboxAgeConfirmation extends React.Component{
   
- render(){
+ state={
+   isConfirmed: false,
+ }
+ handleCheckboxChange =()=>{
+   this.setState({
+   isConfirmed: !this.state.isConfirmed,
+  })
+  
+ }
+  render(){
+    console.log(this.state.isConfirmed);
    return(
+    
     <>
+    
       <h1>Kup bilet na horror roku!</h1>
-      <input type="checkbox" id="age"/>
+      <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
       <label htmlFor="age">Mam co najmniej 16 lat</label>
       
     </> 
